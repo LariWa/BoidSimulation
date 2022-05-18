f587bc3de50e2c601da57fbed5188f005160c5b9
Andrea Maderthaner <andmad@kth.se>
2022-05-19 00:50:21 +0200

    player controls and animation

--- Assets/Scripts/SubController.cs
+++ Assets/Scripts/SubController.cs
@@ -11,16 +11,18 @@ public class SubController : MonoBehaviour
     public float turnSpeed; // The speed the submarine turns left and right
     public float stabilizationSmoothing; // The smoothing applied to the correction turning
     public float riseSpeed; // The speed the submarine rises and lowers
+    public GameObject turbine;
 
     private float curSpeed; // The current stores forwards and backwards speed
     private Rigidbody rb; // Reference to the Rigidbody of the submarine
-    //private SubAnimations subAnimations; // Reference to the submarine animations script
+    public float turbineSpinSpeed; // The speed the turbine spins at max speed
+    public float turbineSmoothing; // The smoothing applied to the changing of the turbine speed
+    private float lastTurbineTurn; // Stores the speed of the turbine from the last frame
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>(); // Getting the Rigidbody reference
-        //subAnimations = GetComponent<SubAnimations>(); // Getting the Sub Animations script reference
     }
 
     // Update is called once per frame
@@ -37,22 +39,22 @@ public class SubController : MonoBehaviour
         if (Input.GetKey(KeyCode.W)) // When the player presses the W key
         {
             curSpeed += speedChangeAmount; // Add to the submarine's speed
-            //subAnimations.Spin(1); // Spin the turbine forwards based on the current speed
+            Spin(1); // Spin the turbine forwards based on the current speed
         }
         else if (Input.GetKey(KeyCode.S)) // When the player presses the W key
         {
             curSpeed -= speedChangeAmount; // Subtract from the submarine's speed
-            //subAnimations.Spin(-1); // Spin the turbine backwards based on the current speed
+            Spin(-1); // Spin the turbine backwards based on the current speed
         }
         else if (Mathf.Abs(curSpeed) <= minSpeed) // If the player is not pressing W or S and the current speed is less than the minumum speed
         {
             curSpeed = 0; // Snap the submarine to not move
-            //subAnimations.Spin(0); // Stop spinning the turbine
+            Spin(0); // Stop spinning the turbine
         }
         else if (curSpeed != 0) // If the player is not pressing W or S but is moving
         {
             curSpeed = 0;
-            //subAnimations.Spin(curSpeed/Mathf.Abs(curSpeed)/2); // Idly spin the turbine based on the current speed
+            Spin(curSpeed/Mathf.Abs(curSpeed)/2); // Idly spin the turbine based on the current speed
         }
         curSpeed = Mathf.Clamp(curSpeed, -maxBackwardSpeed, maxForwardSpeed); // Clamp the current speed based on it's max values in both directions
         rb.AddForce(transform.forward * curSpeed); // Apply the force to the Rigidbody to move the submarine
@@ -63,16 +65,10 @@ public class SubController : MonoBehaviour
         if (Input.GetKey(KeyCode.D)) // When the player presses the D key
         {
             rb.AddTorque(transform.up * turnSpeed); // Apply torque to turn the submarine right
-            //subAnimations.DrillTurn(1); // Turn the drill right
         }
         else if (Input.GetKey(KeyCode.A)) // When the player presses the A key
         {
             rb.AddTorque(transform.up * -turnSpeed); // Apply torque to turn the submarine left
-            //subAnimations.DrillTurn(-1); // Turn the drill left
-        }
-        else // If the player is not turning
-        {
-            //subAnimations.DrillTurn(0); // Stop turning the drill
         }
     }
 
@@ -81,16 +77,10 @@ public class SubController : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftShift)) // When the player presses the Left Shift key
         {
             rb.AddForce(transform.up * riseSpeed); // Apply force to make the submarine rise
-            //subAnimations.FinTurn(-1); // Turn the fins down
         }
         else if (Input.GetKey(KeyCode.LeftControl)) // When the player presses the Left Control key
         {
             rb.AddForce(transform.up * -riseSpeed); // Apply force to make the submarine lower
-            //subAnimations.FinTurn(1); // Turn the fins up
-        }
-        else // If the player is not moving vertically
-        {
-            //subAnimations.FinTurn(0); // Stop turning the fins
         }
     }
 
@@ -105,6 +95,13 @@ public class SubController : MonoBehaviour
       {
           Physics.IgnoreCollision(collision.gameObject.GetComponent<Collider>(), GetComponent<Collider>());
       }
+    }
+
+    public void Spin (float dir)
+    {
+        float curTurn = Mathf.MoveTowards(lastTurbineTurn, turbineSpinSpeed * dir, turbineSmoothing * Time.deltaTime); // Calculate how much to turn the turbine
+        turbine.transform.Rotate(0, 0, curTurn); // Spin the turbine
+        lastTurbineTurn = curTurn; // Store the speed of the turbine to be used next frame
     }
 
 }
